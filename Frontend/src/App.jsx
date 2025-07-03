168ed041a182a3bf48c2bfe8679778c28af78909
lalitmaan920 <lalitmaan2004@gmail.com>
2025-07-03 16:46:20 +0530

    change tho home in pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,24 @@
-import { Routes, Route, Link, Navigate } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
+
+import Home from './Pages/Home'; //
 import Login from './Pages/Login';
 import Signup from './Pages/Signup';
 
 function App() {
   return (
-    <div className='App'>
-    
+    <div className="App">
       <Routes>
-        <Route path="/" element={<Navigate to="login" />} />
-        <Route path="/login" element={<Login/>} />
+        {/* Redirect root to /login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        
+        {/* Auth routes */}
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home/>} />
+
+        {/* Main app route */}
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
